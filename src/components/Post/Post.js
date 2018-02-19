35f6ee005396348974769dcc4c0be93d61a16d76
sebaural <uralsebastian@gmail.com>
2018-02-18 23:02:41 -0500

    printing out posts

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,12 +3,25 @@ import React from 'react';
 import './Post.css';
 
 const post = (props) => (
-    <article className="Post">
-        <h1>Title</h1>
-        <div className="Info">
-            <div className="Author">Author</div>
-        </div>
-    </article>
+  <article className="Post" onClick={props.postClicked} >
+
+    <div className="post_container">
+      <div className="post-img">
+        <img src={props.url} alt=""/>
+      </div>
+      <div className="title_holder">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="info-holder">
+        <span>
+          {props.author}
+        </span>
+        <span>
+          {props.created_utc}
+        </span>
+      </div>
+    </div>
+  </article>
 );
 
 export default post;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
+/*
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+*/
 import './Blog.css';
 
 class Blog extends Component {
+
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios.get('https://www.reddit.com/r/analog/top/.json')
+      .then(response => {
+        const mapPosts = response.data.data.children.map(posts => {
+          console.log(posts);
+          return { ...posts }
+          }
+        );
+
+        this.setState({posts: mapPosts});
+        console.log(this.state.posts);
+      });
+  };
+
     render () {
+
+      const posts = this.state.posts.map(post => {
+        return <Post
+          key={post.data.id}
+          url={post.data.url}
+          title={post.data.title}
+          author={post.data.author}
+          created_utc={post.data.created_utc}
+        />
+      });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                  {posts}
                 </section>
+
+{/*
                 <section>
                     <FullPost />
                 </section>
                 <section>
                     <NewPost />
                 </section>
+*/}
             </div>
         );
     }
