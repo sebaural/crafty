1aef4b4a0c9f7e26ea696fd20cccf50704785d29
sebaural <uralsebastian@gmail.com>
2018-02-20 00:40:58 -0500

    moved post related funcs into posts.js

---
+++ src/containers/Blog/Posts/posts.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+class Posts extends React.Component {
+
+  state = {
+    posts: [],
+    favedPosts: []
+  };
+
+  componentDidMount() {
+    /*axios.get('https://www.reddit.com/r/analog/top/.json')*/
+    axios.get('/.json')
+      .then(response => {
+        const mapPosts = response.data.data.children.map(posts => {
+            return { ...posts }
+          }
+        );
+
+        this.setState({posts: mapPosts});
+      });
+  };
+
+  selectedFave = (id) => {
+    const favesArr = this.state.favedPosts;
+    const faveId = id;
+    const inArr = favesArr.indexOf(faveId);
+    if (inArr === -1) {
+      favesArr.push(faveId);
+    }
+    this.setState({favedPosts: favesArr});
+    console.log(favesArr);
+  };
+
+
+  render() {
+    const posts = this.state.posts.map(post => {
+
+      let createdTime = post.data.created;
+      let postDate = new Date(createdTime).toDateString();
+
+      return <Post
+        key={post.data.id}
+        url={post.data.url}
+        title={post.data.title}
+        author={post.data.author}
+        created={postDate}
+        ups={post.data.ups}
+        postClicked={() => this.selectedFave(post.data.id)}
+      />
+    });
+
+    return (
+      <section className="Posts">
+        <h2 className="page-title">Look at these posts</h2>
+        {posts}
+      </section>
+    );
+
+  }
+}
+
+
+export default Posts;
+
+
